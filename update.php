70d7d60189369136fb20ca8fe517d19e6283fd5e
mlisondra <milder.lisondra@lisondraconsulting.com>
2015-08-14 12:54:41 -0700

    Deleted
    Proper version in /admin

--- update.php
+++ update.php
@@ -10,12 +10,6 @@ if($action == "update"){
 	
 	$LiveChatAPI = new LiveChat('web@jewsforjesus.org', '5e191e7817a1186db337627593cab804');
 	$user_login = $user_login;
-	/*
-if($login_status == "not accepting chats"){
-	$status = "offline";
-}else{
-	$status = "accepting chats";
-}*/
 	$user_updates = array("login_status"=>"not accepting chats","status"=>$status);
 	$result = $LiveChatAPI->agents->update($user_login, $user_updates);
 	print_r($result);
